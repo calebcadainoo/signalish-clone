82a024dce07b651b4181b3774a23d4c05396ee5b
Caleb Crosby Adainoo <code.crosby@gmail.com>
2021-05-01 23:54:07 +0000

    feat: setup react-native navigation

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyCjze4IehE7lRzPjGWiMdjPYNRgUICulPg",
+	authDomain: "signalish-clone.firebaseapp.com",
+	projectId: "signalish-clone",
+	storageBucket: "signalish-clone.appspot.com",
+	messagingSenderId: "549884888076",
+	appId: "1:549884888076:web:f1e4e9aea461fb86501a29",
+	measurementId: "G-HH88MVGQ59",
+};
