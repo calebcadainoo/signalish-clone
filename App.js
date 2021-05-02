8cb049793ef4bb8931bd84d52d308aee97a98b07
Caleb Crosby Adainoo <code.crosby@gmail.com>
2021-05-02 00:36:47 +0000

    feat: Login - add screen to navigation stack, apply general style to pages

--- App.js
+++ App.js
@@ -3,14 +3,23 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+	headerStyle: { backgroundColor: "#2C6BED" },
+	headerTitleStyle: { color: "white" },
+	headerTintColor: "white",
+};
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<View style={styles.container}>
-				<Text>So sweet I'm finally here THANK GOD MAN</Text>
-				<StatusBar style="auto" />
-			</View>
+			<Stack.Navigator screenOptions={globalScreenOptions}>
+				<Stack.Screen name="Login" component={LoginScreen} />
+			</Stack.Navigator>
 		</NavigationContainer>
 	);
 }
