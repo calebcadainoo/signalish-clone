c5cab0e8efb887e11834bad4d23a0666c6b53fab
Caleb Crosby Adainoo <code.crosby@gmail.com>
2021-05-02 15:54:34 +0000

    feat: Firebase Setup - connect app config to firebase

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,3 +1,11 @@
+import * as firebase from "firebase";
+// Optionally import the services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+//import "firebase/database";
+//import "firebase/functions";
+//import "firebase/storage";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: "AIzaSyCjze4IehE7lRzPjGWiMdjPYNRgUICulPg",
@@ -8,3 +16,14 @@ const firebaseConfig = {
 	appId: "1:549884888076:web:f1e4e9aea461fb86501a29",
 	measurementId: "G-HH88MVGQ59",
 };
+
+let app;
+if (firebase.apps.length === 0) {
+	app = firebase.initializeApp(firebaseConfig);
+} else {
+	app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+export { db, auth };
