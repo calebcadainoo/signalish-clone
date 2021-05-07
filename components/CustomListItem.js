4f2ac3ee2d967f6587416275c2eb7795ca5761db
Caleb Crosby Adainoo <code.crosby@gmail.com>
2021-05-07 19:41:27 +0000

    feat: CustomListItems complete layout of component

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { ListItem, Avatar } from "react-native-elements";
 
-const CustomListItem = () => {
+const CustomListItem = ({ id, chatName, funcEnterChat }) => {
 	return (
 		<ListItem>
 			<Avatar
@@ -12,7 +12,19 @@ const CustomListItem = () => {
 						"https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png",
 				}}
 			/>
-			<Text>Custom List elements</Text>
+			<ListItem.Content>
+				<ListItem.Title
+					style={{ fontWeight: "600" }}
+					numberOfLines={1}
+					ellipsizeMode="tail"
+				>
+					Crosby Chat Room
+				</ListItem.Title>
+
+				<ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
+					Hello y'all
+				</ListItem.Subtitle>
+			</ListItem.Content>
 		</ListItem>
 	);
 };
